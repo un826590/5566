33c39a5a7c9f0ab3943e5720ab44ce19174cc644
un826590 <78077478+un826590@users.noreply.github.com>
2021-01-27 18:01:02 +0800

    Add files via upload

---
+++ statics1/js/playerconfig.js
@@ -0,0 +1,7 @@
+﻿var MacPlayerConfig={};
+//参数开始
+MacPlayerConfig={"width":"100%","height":"400px","widthmob":"100%","heightmob":"100%","widthpop":"0","heightpop":"600","second":"5","prestrain":"\/\/union.maccms.com\/html\/prestrain.html","buffer":"\/\/union.maccms.com\/html\/buffer.html","parse":"\/\/api.maccms.com\/parse\/?url=","autofull":"0","showtop":"1","showlist":"1","flag":"0","colors":"000000,F6F6F6,F6F6F6,333333,666666,FFFFF,FF0000,2c2c2c,ffffff,a3a3a3,2c2c2c,adadad,adadad,48486c,fcfcfc"};
+//参数结束
+//缓存开始
+MacPlayerConfig.player_list={"mahua":{"show":"\u5feb\u732b\u7ebf\u8def","des":"https:\/\/www.mhbofang.com","ps":"1","parse":"https:\/\/www.mhbofang.com\/?url="},"123kum3u8":{"show":"\u9177\u4e91\u7ebf\u8def","des":"https:\/\/www.mhbofang.com","ps":"1","parse":"https:\/\/www.mhbofang.com\/?url="}},MacPlayerConfig.downer_list={"http":{"show":"http\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""},"xunlei":{"show":"xunlei\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""}},MacPlayerConfig.server_list={"server1":{"show":"\u6d4b\u8bd5\u670d\u52a1\u56681","des":"des\u63d0\u793a\u4fe1\u606f1"}};
+//缓存结束
